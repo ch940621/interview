d03bba377d48b6c91b448e0f81583b42d7d42ad2
陈浩 <chenhao@chenhaodeMacBook-Pro.local>
2018-06-29 20:05:52 +0800

    1

--- 链表/合并两个有序链表和合并N个有序链表.py
+++ 链表/合并两个有序链表和合并N个有序链表.py
@@ -4,7 +4,7 @@ class Node(object):
         self.next = None
 
 
-def merge_two_linklist(head1,head2):
+def merge_two_linklist(head1, head2):
     if head1 == None or head2 == None:
         return head1 if head1 != None else head2
     new_head = head1 if head1.value <= head2.value else head2
@@ -12,8 +12,8 @@ def merge_two_linklist(head1,head2):
     if new_head == head1:
         head1 = head1.next
     else:
-        head2 =head2.next
-    
+        head2 = head2.next
+
     tail = new_head
 
     while head1 != None and head2 != None:
@@ -29,11 +29,12 @@ def merge_two_linklist(head1,head2):
         tail.next = head2
     elif head2 == None:
         tail.next = head1
-    
+
     return new_head
 
+
 # 递归版本
-def merge_two_linklist1(head1,head2):
+def merge_two_linklist1(head1, head2):
     new_head = None
     if head1 == None:
         return head2
@@ -42,10 +43,10 @@ def merge_two_linklist1(head1,head2):
     else:
         if head1.value < head2.value:
             new_head = head1
-            new_head.next = merge_two_linklist1(head1.next,head2)
+            new_head.next = merge_two_linklist1(head1.next, head2)
         else:
             new_head = head2
-            new_head.next = merge_two_linklist1(head1,head2.next)
+            new_head.next = merge_two_linklist1(head1, head2.next)
         return new_head
 
 
@@ -54,12 +55,10 @@ def merge_n_linklist(lists):
     if num == 0:
         return None
     while num > 1:
+        k = (num + 1) // 2
         for pos in range(num // 2):
-            pass
-             
-
-
-
+            lists[pos] = merge_two_linklist(lists[pos], lists[pos + k])
+        num = k
 
 
 # TODO
